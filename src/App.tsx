857823cc92e5296d14307a20133b0d4c3431b60b
YukihiroEhara <yukihiro.ehara@brandcloud.co.jp>
2022-07-05 16:10:22 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>GitHub Actions Test!!</p>
+      <p>GitHub Actions Test!!!!</p>
       <p>commitTest</p>
     </div>
   );
