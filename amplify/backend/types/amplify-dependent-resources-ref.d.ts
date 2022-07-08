459093cca8440afa2fb318ae8ecabc7c2e4690a1
YukihiroEhara <yukihiro.ehara@brandcloud.co.jp>
2022-07-08 16:03:39 +0900

    amplify console check commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "cicdtestapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
