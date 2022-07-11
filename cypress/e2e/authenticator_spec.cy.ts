a4cbb073a3e6ee5ca8880b95f219b9c1a3653930
YukihiroEhara <yukihiro.ehara@brandcloud.co.jp>
2022-07-11 18:05:12 +0900

    export push

--- cypress/e2e/authenticator_spec.cy.ts
+++ cypress/e2e/authenticator_spec.cy.ts
@@ -1,6 +1,6 @@
 describe("Authenticator:", function () {
   beforeEach(function () {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://localhost:3000", { timeout: 30000 });
   });
   describe("Text Check", () => {
     it("My First Test", () => {
