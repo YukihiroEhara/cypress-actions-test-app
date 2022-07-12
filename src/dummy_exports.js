24daff491a80cb8809186fe720b28054dfb25a04
YukihiroEhara <yukihiro.ehara@brandcloud.co.jp>
2022-07-12 17:12:50 +0900

    test

--- src/dummy_exports.js
+++ src/dummy_exports.js
@@ -12,8 +12,8 @@ const awsmobile = {
   aws_appsync_graphqlEndpoint:
     process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT,
   aws_appsync_region: process.env.REACT_APP_AWS_REGION,
-  //   aws_appsync_authenticationType:
-  //     process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
+  aws_appsync_authenticationType:
+    process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
   //   aws_appsync_apiKey: process.env.REACT_APP_AWS_APPSYNC_APIKEY,
   aws_cognito_identity_pool_id:
     process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
